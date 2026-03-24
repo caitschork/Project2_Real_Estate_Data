26b684070c969406ff3fc61f61afb9284fc0d9d8
SVargo7 <149019304+SVargo7@users.noreply.github.com>
2026-03-23 21:34:45 -0400

    Create QuickSort.h, define funcs

---
+++ QuickSort.h
@@ -0,0 +1,12 @@
+//
+// Created by Sydney Vargo on 3/23/26.
+//
+#include <vector>
+
+#ifndef PROJECT2_QUICKSORT_H
+#define PROJECT2_QUICKSORT_H
+
+void quickSort(std::vector<realEstate> listings, int low, int high);
+int partition(std::vector<realEstate> listings, int low, int high);
+
+#endif //PROJECT2_QUICKSORT_H
