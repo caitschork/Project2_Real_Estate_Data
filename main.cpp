c336083d04b79c4655746be8be5545f82db9a05e
caitschork <caitschork@gmail.com>
2026-03-23 21:25:01 -0400

    Committing before adding Python plots

--- main.cpp
+++ main.cpp
@@ -28,7 +28,7 @@ vector<realEstate> filterHouses(const vector<realEstate>& houses, double maxPric
             (house_size == 0 || house.house_size >= house_size) &&
             (beds == 0 || house.beds == beds) &&
             (baths == 0 || house.baths == baths) &&
-            house.price > 0 && house.house_size > 0) {
+            house.price > 1 && house.house_size > 1) {
             filteredHouses.push_back(house);
         }
     }
@@ -36,9 +36,6 @@ vector<realEstate> filterHouses(const vector<realEstate>& houses, double maxPric
 }
 
 int main() {
-    // vector<double> y = {1, 3, 2, 5, 4};
-    // plot(y);
-    // show();
     vector<realEstate> data = loadData("../realtor-data-new.csv");
 
     if (data.empty()) {
@@ -54,14 +51,6 @@ int main() {
     double house_size;
     int beds;
     int baths;
-    //
-    // // for (int i = 10000; i < 10004; i++) {
-    // //     cout << "Loaded Price: " << data[i].price << endl;
-    // // }
-    //
-    // cout << "Enter max price (or 0 to skip): ";
-    // cin >> maxPrice;
-
 
     cout << "Enter max price (or 0 to skip): ";
     cin >> maxPrice;
@@ -120,78 +109,14 @@ int main() {
 
     cout << "\nMerge Sort Time: " << mergeTime << " ms" << endl;
 
-    //
-    // cout << "Enter city (or NONE to skip): ";
-    // cin >> city;
-    //
-
-    // cout << "Enter state (or NONE to skip): ";
-    // cin >> state;
-
     // auto start3 = std::chrono::high_resolution_clock::now();
     // quickSort(quickData, 0, quickData.size() - 1, sortChoice);
     // auto end3 = std::chrono::high_resolution_clock::now();
     // auto quickTime = std::chrono::duration_cast<std::chrono::milliseconds>(end3 - start3).count();
 
-    //
-    // cout << "Enter zip code (or NONE to skip): ";
-    // cin >> zip_code;
-    //
-    // cout << "Enter minimum acre lot size (or 0 to skip): ";
-    // cin >> acre_lot;
-    //
-    // cout << "Enter minimum house size in square feet (or 0 to skip): ";
-    // cin >> house_size;
-    //
-    // cout << "Enter the number of bedrooms (or 0 to skip): ";
-    // cin >> beds;
-    //
-    // cout << "Enter number of bathrooms (or 0 to skip): ";
-    // cin >> baths;
-    //
-    // vector <realEstate> filtered = filterHouses(data, maxPrice, city, state, zip_code, acre_lot, house_size, beds, baths);
-    //
-    // cout << "\nFiltered houses dataset size: " << filtered.size() << endl;
-    //
-    // int sortChoice;
-    // cout << "\nSort by: "
-    //         "\n1. House Price"
-    //         "\n2. House Size"
-    //         "\n3. Number of Beds"
-    //         "\n4. Number of Baths"
-    //         "\n5. Acre Lot Size"
-    //         "\nChoice: ";
-    // cin >> sortChoice;
-    //
-    // vector<realEstate> heapData = filtered;
-    //
-    // auto start1 = std::chrono::high_resolution_clock::now();
-    // heapSort(heapData, sortChoice);
-    // auto end1 = std::chrono::high_resolution_clock::now();
-    // auto heapTime = std::chrono::duration_cast<std::chrono::milliseconds>(end1 - start1).count();
-    //
-    // cout << "\nHeap Sort Time: " << heapTime << " ms" << endl;
-    //
-    // //other sorts to be implemented
-    // vector<realEstate> mergeData = filtered;
-    //
-    // auto start2 = std::chrono::high_resolution_clock::now();
-    // mergeSort(mergeData, 0, mergeData.size() - 1, sortChoice);
-    // auto end2 = std::chrono::high_resolution_clock::now();
-    // auto mergeTime = std::chrono::duration_cast<std::chrono::milliseconds>(end2 - start2).count();
-    //
-    // cout << "\nMerge Sort Time: " << mergeTime << " ms" << endl;
-    // //
-    // // vector<realEstate> quickData = filtered;
-    // //
-    // // auto start3 = std::chrono::high_resolution_clock::now();
-    // // quickSort(mergeData, 0, quickData.size() - 1, sortChoice);
-    // // auto end3 = std::chrono::high_resolution_clock::now();
-    // // auto quickTime = std::chrono::duration_cast<std::chrono::milliseconds>(end3 - start3).count();
-    // //
-    // // cout << "\nQuick Sort Time: " << quickTime << " ms" << endl;
-    //
-    //
+    // cout << "\nQuick Sort Time: " << quickTime << " ms" << endl;
+
+
     cout << "\nTop 5 Results:\n";
     // for (int i = heapData.size() - 1; i >= 0 && i >= heapData.size() - 5; i--) {
     for (int i = 0; i < 5 && i < heapData.size(); i++) {
@@ -205,43 +130,8 @@ int main() {
         cout << "House Size: " << heapData[i].house_size << "\n";
         cout << "\n";
     }
-    //
-    // //graphics
-    // // vector<string> labels = {"Heap", "Merge", "Quick"};
-    // // vector<double> times = {(double)heapTime, (double)mergeTime, (double)quickTime};
-    // //
-    // // figure();
-    // // bar(times);
-    // // xticks({0, 1, 2});
-    // // xticklabels(labels);
-    // // tite("Sorting Algorithms Runtime");
-    // // ylabel("Time (ms)");
-    // // show();
-    // vector<double> prices;
-    //
-    // for (const auto& house : heapData) {
-    //     prices.push_back(house.price);
-    // }
-    //
-    // figure();
-    // plot(prices);
-    // title("Sorted House Prices");
-    // xlabel("Index");
-    // ylabel("Price");
-    // show();
+
+
     return 0;
 }
 
-//Print first 5 rows
-// for (int i = 0; i < 5 && i < data.size(); i++) {
-//     cout << "House " << i + 1 << ":\n";
-//     cout << "Price: " << data[i].price << "\n";
-//     cout << "Beds: " << data[i].beds << "\n";
-//     cout << "Baths: " << data[i].baths << "\n";
-//     cout << "Acre Lot: " << data[i].acre_lot << "\n";
-//     cout << "City: " << data[i].city << "\n";
-//     cout << "State: " << data[i].state << "\n";
-//     cout << "Zip: " << data[i].zip_code << "\n";
-//     cout << "House Size: " << data[i].house_size << "\n";
-//     cout << "\n\n";
-// }
